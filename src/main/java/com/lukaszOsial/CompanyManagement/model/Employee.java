355b61b65f71221413689c4c6d60b8f024a4f00c
Łukasz Osial <lukasz.osial@gmail.com>
2023-01-26 10:51:49 +0100

    commit

--- src/main/java/com/lukaszOsial/CompanyManagement/model/Employee.java
+++ src/main/java/com/lukaszOsial/CompanyManagement/model/Employee.java
@@ -1,11 +1,34 @@
 package com.lukaszOsial.CompanyManagement.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     private String email;
+    @Column(name = "salary")
     private int salary;
+
+    public Employee() {
+
+    }
+    public Employee(String firstName, String lastName, String email, int salary) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.salary = salary;
+    }
 
     public int getId() {
         return id;
