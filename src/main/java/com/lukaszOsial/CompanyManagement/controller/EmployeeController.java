06e1ae075b636955580f905284d6f31c167154ec
Łukasz Osial <lukasz.osial@gmail.com>
2023-01-29 11:04:09 +0100

    commit

--- src/main/java/com/lukaszOsial/CompanyManagement/controller/EmployeeController.java
+++ src/main/java/com/lukaszOsial/CompanyManagement/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.lukaszOsial.CompanyManagement.controller;
 
+import com.lukaszOsial.CompanyManagement.exception.ResourceNotFoundException;
 import com.lukaszOsial.CompanyManagement.model.Employee;
 import com.lukaszOsial.CompanyManagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +25,27 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie istnieje pracownik o podanym id: " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Integer id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie istnieje pracownik o podanym id: " + id));
+        
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setSalary(employeeDetails.getSalary());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 }
